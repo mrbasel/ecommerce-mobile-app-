a28880c8721e969b60239838a5bce043188138e4
Basel Al Nassr <mrbasel555@gmail.com>
2020-06-25 15:55:14 +0300

    Finished item screen

--- lib/screens/item_screen.dart
+++ lib/screens/item_screen.dart
@@ -23,29 +23,66 @@ class ItemScreen extends StatelessWidget {
           ),
         ),
         body: Container(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Container(
-                child: Image.network(item.imgUrl),
-                width: 200,
-                margin: EdgeInsets.all(20),
+          child: Stack(children: [
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    child: Image.network(item.imgUrl),
+                    width: 200,
+                    margin: EdgeInsets.all(20),
+                  ),
+                  Text(
+                    item.name,
+                    style: TextStyle(fontSize: 25, color: Colors.grey[600]),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        item.price,
+                        textAlign: TextAlign.left,
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
+                      )),
+                ],
               ),
-              Text(item.name, style: TextStyle(fontSize: 25, color: Colors.grey[600]), ),
-              SizedBox(height: 10,),
-              Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    item.price,
-                    textAlign: TextAlign.left,
-                    style: TextStyle(
-                      fontSize: 20, 
-                      fontWeight: FontWeight.bold
-                    ),
-                  )),
-            ],
-          ),
-          margin: EdgeInsets.symmetric(horizontal: 20),
+            ),
+            Positioned(
+              child: FlatButton(
+                child: Text(
+                  'Add To My Cart',
+                  style: TextStyle(color: Colors.white),
+                ),
+                padding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+                color: Colors.blue[800],
+                onPressed: () => print('Clicked!'),
+              ),
+              bottom: 3,
+              left: 5,
+              right: 100,
+            ),
+            Positioned(
+              child: Tooltip(
+                message: 'Add To Wishlist',
+                child: FlatButton(
+                  child: Icon(
+                    Icons.favorite_border,
+                    color: Colors.white,
+                  ),
+                  padding: EdgeInsets.symmetric(horizontal: 25, vertical: 12),
+                  color: Colors.red,
+                  onPressed: () => print('Clicked!'),
+                ),
+              ),
+              bottom: 3,
+              right: 5,
+            ),
+          ]),
         ));
   }
 }
