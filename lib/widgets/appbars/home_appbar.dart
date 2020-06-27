af7b6fee7299694f0e476e55237ace08a33eda7b
Basel Al Nassr <mrbasel555@gmail.com>
2020-06-27 15:23:59 +0300

    Added badge item counter

--- lib/widgets/appbars/home_appbar.dart
+++ lib/widgets/appbars/home_appbar.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:badges/badges.dart';
 
 import 'package:ecom_app/screens/cart_screen.dart';
 
-class HomeAppBar extends StatefulWidget implements PreferredSizeWidget {
-  HomeAppBar({Key key, this.title, this.color})
+class MainAppBar extends StatefulWidget implements PreferredSizeWidget {
+  MainAppBar({Key key, this.title, this.color, this.elevation, this.showCart})
       : preferredSize = Size.fromHeight(kToolbarHeight),
         super(key: key);
 
@@ -11,31 +13,45 @@ class HomeAppBar extends StatefulWidget implements PreferredSizeWidget {
   final Size preferredSize; // default is 56.0
   final String title;
   final Color color;
+  final elevation;
+  final bool showCart;
 
   @override
-  _HomeAppBarState createState() => _HomeAppBarState();
+  _MainAppBarState createState() => _MainAppBarState();
 }
 
-class _HomeAppBarState extends State<HomeAppBar> {
+class _MainAppBarState extends State<MainAppBar> {
   @override
   Widget build(BuildContext context) {
     return AppBar(
-      iconTheme: IconThemeData(color: Colors.black),
-      centerTitle: true,
-      title: Text(
-        widget.title,
-        style: TextStyle(color: Colors.black),
-      ),
-      backgroundColor: widget.color,
-      actions: <Widget>[
-        IconButton(
-            icon: Icon(
-              Icons.shopping_cart,
-              color: Colors.white,
-            ),
-            onPressed: () => Navigator.push(
-                context, MaterialPageRoute(builder: (context) => CartScreen())))
-      ],
-    );
+        iconTheme: IconThemeData(color: Colors.black),
+        centerTitle: true,
+        elevation: widget.elevation,
+        title: Text(
+          widget.title,
+          style: TextStyle(color: Colors.black),
+        ),
+        backgroundColor: widget.color,
+        actions: <Widget>[
+          widget.showCart == false
+              ? SizedBox()
+              : IconButton(
+                  icon: StreamBuilder(
+                      stream:
+                          Firestore.instance.collection('items').snapshots(),
+                      builder: (context, snapshot) {
+                        if (!snapshot.hasData) return SizedBox();
+                        return Badge(
+                          badgeContent:
+                              Text(snapshot.data.documents.length.toString()),
+                          child: Icon(
+                            Icons.shopping_cart,
+                            color: Colors.black,
+                          ),
+                        );
+                      }),
+                  onPressed: () => Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => CartScreen())))
+        ]);
   }
 }
