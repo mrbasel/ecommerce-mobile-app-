7a370cccb473e652201e8650eb02c6ac68a56946
Basel <mrbasel555@gmail.com>
2020-07-13 18:32:14 +0300

    Added items stream to home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,11 +1,13 @@
 import 'package:carousel_slider/carousel_options.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 
 import 'package:ecom_app/widgets/appbars/home_appbar.dart';
 import 'package:ecom_app/widgets/drawers/main_drawer.dart';
 import 'package:ecom_app/widgets/item_card.dart';
-import 'package:ecom_app/models/data.dart';
+import 'package:ecom_app/models/models.dart';
+
 
 class Home extends StatelessWidget {
   
@@ -33,20 +35,29 @@ class Home extends StatelessWidget {
                   ),
                 ]),
             Container(
-              child: ListView.builder(
-                  shrinkWrap: true,
-                  physics: const ClampingScrollPhysics(),
-                  itemCount: items.length,
-                  itemBuilder: (context, int index) {
-                    return ItemCard(
-                      showCart: true,
-                      item: items[index],
-                      trailingWidget: SizedBox(
-                        width: 20.0,
-                        height: 20.0,
-                      ),
-                    );
-                  }),
+              child: StreamBuilder<QuerySnapshot>(
+                stream: Firestore.instance.collection('items').snapshots(),
+                builder: (context, snapshot) {
+                  return ListView.builder(
+                      shrinkWrap: true,
+                      physics: const ClampingScrollPhysics(),
+                      itemCount: snapshot.data.documents.length,
+                      itemBuilder: (context, int index) {
+                        return ItemCard(
+                          showCart: true,
+                          item: Item(
+                           name:  snapshot.data.documents[index].data['name'],
+                           imgUrl: snapshot.data.documents[index].data['imgUrl'],
+                           price: snapshot.data.documents[index].data['price']
+                          ),
+                          trailingWidget: SizedBox(
+                            width: 20.0,
+                            height: 20.0,
+                          ),
+                        );
+                      });
+                }
+              ),
               margin: EdgeInsets.only(top: 60),
             )
           ],
